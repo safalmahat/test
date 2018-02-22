91d95b83833bc4da9e6584bfcae188a6ecb98beb
Safal Mahat <safal1mahat@gmail.com>
2018-02-22 09:24:49 +0545

    filter added

--- WebApplication2/WebApplication2/Repo/CrudRepo.cs
+++ WebApplication2/WebApplication2/Repo/CrudRepo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using WebApplication2.Models;
-
+using System.Reflection;
 namespace WebApplication2.Repo
 {
     public class CrudRepo<TModel> : ICrudRepo<TModel> where TModel : class, new()
@@ -12,25 +12,28 @@ namespace WebApplication2.Repo
         {
             using (EventMgmtSystemEntities ctx = new EventMgmtSystemEntities())
             {
-                ctx.Set<TModel>().Add(data);
-                int result = ctx.SaveChanges();
-                if (result > 0)
-                    return true;
-                else
-                    return false;
-                //if (data.Id > 0)
-                //{
-                //    ctx.Entry(data).State = System.Data.Entity.EntityState.Modified;
-                //    int result = ctx.SaveChanges();
-                //    if (result > 0)
-                //        return true;
-                //    else
-                //        return false;
-                //}
-                //else
-                //{
 
-                //}
+               PropertyInfo prop= data.GetType().GetProperty("Id");
+             int id= Convert.ToInt32(prop.GetValue(data));
+                if (id>0)
+                {
+                    ctx.Entry(data).State = System.Data.Entity.EntityState.Modified;
+                    int result = ctx.SaveChanges();
+                    if (result > 0)
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                {
+                    ctx.Set<TModel>().Add(data);
+                    int result = ctx.SaveChanges();
+                    if (result > 0)
+                        return true;
+                    else
+                        return false;
+
+                }
 
             }
         }
--- WebApplication2/WebApplication2/Repo/ICrudRepo.cs
+++ WebApplication2/WebApplication2/Repo/ICrudRepo.cs
@@ -8,7 +8,7 @@ namespace WebApplication2.Repo
 {
     interface ICrudRepo<TModel>  where TModel:class,new()
     {
-        //List<user>
+        //List<user> 
         bool Add(TModel data);
         bool Update(TModel data);
         TModel Get(int id);
